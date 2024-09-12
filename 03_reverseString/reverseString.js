d7ecdae821c98a2aa8e0f9b1e35067ec581ac544
PrajapatiBhavin <172618328+PrajapatiBhavin@users.noreply.github.com>
2024-09-12 19:18:43 +0530

    Update the code for exercise-03 to ensure it hadles punctuations as well

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,9 +1,18 @@
-const reverseString = function(str) {
+/*
+const reverseString = function (str) {
     const arrStr = str.split('');
     let revArrStr = arrStr.reverse();
     revArrStr = revArrStr.toString();
-    return revArrStr.replace(/,/g , ''); //Anything between /xyx/g will be replaced for string. This is global
-    };
+    return revArrStr.replace(/,/g, ''); //Anything between /xyx/g will be replaced for string. This is global
+};
 
+*/ 
+//This solution is more generic as it also handle punctuations. Note ".join()" method.
+const reverseString = function (str) {
+    const arrStr = str.split('');
+    let revArrStr = arrStr.reverse();
+    //revArrStr = revArrStr.toString();
+    return revArrStr.join(''); //Anything between /xyx/g will be replaced for string. This is global
+};
 // Do not edit below this line
 module.exports = reverseString;
