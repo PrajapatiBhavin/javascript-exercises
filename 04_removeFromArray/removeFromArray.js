7826c9c7d51888bdcb025feafbcb8eb7f6a9840a
PrajapatiBhavin <172618328+PrajapatiBhavin@users.noreply.github.com>
2024-09-12 20:41:17 +0530

    Used nest for loops as solution. All test cases passed!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+    const mainArr = arguments[0];
+    let resultArr = mainArr;
+    for (let i = 1; i < arguments.length; i++) {
+        for (let j = 0; j < resultArr.length; j++) {
+            if (resultArr[j] === arguments[i]) {
+                resultArr.splice(j, 1); //Due to splice, a new array is created with revised index. Thus, needs to go one index back.
+                j--;
+            }
+            //else {
+            //    resultArr.push(j);
+            //}
+        }
+
+    }
+    return resultArr;
 };
 
 // Do not edit below this line
