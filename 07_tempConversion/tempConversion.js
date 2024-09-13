a60458cc75f6a9929f5a0d806f007e9c262ddcb1
PrajapatiBhavin <172618328+PrajapatiBhavin@users.noreply.github.com>
2024-09-13 16:40:53 +0530

    Solution for excercis-07temopCoversion. All test cases passed.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,24 @@
-const convertToCelsius = function() {
+let resultInCelsius = 0.0;
+let resultInFahrenheit = 0.0
+const convertToCelsius = function(tempInFahrenheit) {
+  resultInCelsius = ((5 * tempInFahrenheit) - 160) / 9;
+  if (Number.isInteger(resultInCelsius)){
+    return resultInCelsius;
+  }
+  else{
+    return parseFloat(resultInCelsius.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+  resultInFahrenheit = ((9 * tempInCelsius) + 160) / 5;
+  if (Number.isInteger(resultInFahrenheit)){
+    return resultInFahrenheit;
+  }
+  else{
+    return parseFloat(resultInFahrenheit.toFixed(1));
+  }
+  
 };
 
 // Do not edit below this line
